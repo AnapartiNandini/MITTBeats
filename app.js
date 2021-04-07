fc50dcc7deb16fa57ad4f589dccd1e557f31cf3b
Aryan Saini <aryansaini1005@gmail.com>
2021-04-07 14:40:48 -0500

    added the itunes song feature working on lyrics next

--- app.js
+++ app.js
@@ -2,26 +2,35 @@
 const lyrics = document.querySelector('.lyrics')
 const form = document.querySelector('form');
 const searchBar = document.querySelector('#search-bar');
+const baseUrl = { shaz: "https://shazam.p.rapidapi.com/", genius: "https://genius.p.rapidapi.com/", lyrics: "https://api.lyrics.ovh/v1/", download:""};
+const iframe = document.querySelector("iframe");
+const urlHeaders = {genius1: {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "2247980046msh68a22faa7c63535p109a44jsn81484c7d80bf",
+		"x-rapidapi-host": "genius.p.rapidapi.com"
+	}}, 
+ genius2: {
+  "method": "GET",
+  "headers": {
+    "x-rapidapi-key": "4616b7ae9cmshb6e506f5ffff27ep1fe324jsn702dc6119fbe",
+    "x-rapidapi-host": "genius.p.rapidapi.com"
+  }}
+}
 
 async function getArtists(str) {
-  console.log(str);
   str = str.split(" ").join("%20");
 
-  let data = await fetch(`https://genius.p.rapidapi.com/search?q=${str}`, {
-    "method": "GET",
-    "headers": {
-      "x-rapidapi-key": "4616b7ae9cmshb6e506f5ffff27ep1fe324jsn702dc6119fbe",
-      "x-rapidapi-host": "genius.p.rapidapi.com"
-    }
-  });
+  let data = await fetch(`${baseUrl.genius}search?q=${str}`, urlHeaders.genius2);
   data = await data.json();
   lyrics.innerHTML = "";
   data.response.hits.forEach(song => {
     let songName = song.result.title_with_featured;
     let artistName = song.result.primary_artist.name;
     let images = song.result.header_image_thumbnail_url;
+    let id = song.result.id;
       lyrics.innerHTML += `
-      <ul>
+      <ul data-id="${id}">
         <img src = "${images}"/>
         <li class="song-name">
         ${songName}
@@ -29,9 +38,20 @@ async function getArtists(str) {
         <li class="artist-name">
         ${artistName}
         </li>
+        <li class="song-name">
+        <i class="fas fa-play"></i>
+        </li>
       </ul>
       `
   });
+}
+
+async function playMusicSample(id){
+  let data = await fetch(`${baseUrl.genius}songs/${id}`, urlHeaders.genius1);
+  data = await data.json();
+  console.log(data);
+  console.log(data.response.song.apple_music_player_url);
+  iframe.src = `${data.response.song.apple_music_player_url}`;
 }
 
 searchBar.onkeyup  = (e) => {
@@ -47,3 +67,17 @@ form.onsubmit = (e) => {
   e.preventDefault();
 }
 
+lyrics.onclick = (e) => {
+  console.log(e.target);
+  let close = e.target.closest("ul");
+  console.log(close);
+  if (close != undefined && e.target.classList.contains("fa-play")) {
+    playMusicSample(close.dataset.id);
+    e.target.classlist.toggle("fa-play", "fa-pause");
+
+  } else if (close != undefined && e.target.classList.contains("fa-pause")){
+    e.target.classlist.toggle("fa-play", "fa-pause");
+  } else if (close != undefined ){
+
+  }
+}
