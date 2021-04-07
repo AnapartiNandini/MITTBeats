1b6b3a46c40200f9ca59fc7679e0a808a008467a
halimatayoade <43046863+halimatayoade@users.noreply.github.com>
2021-04-07 13:51:26 -0500

    Created Top 10's list

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ async function getArtists() {
 
 getArtists();
 
-async function displayLyrics(song, artist, images) {
+async function displaySongs(song, artist, images) {
   for(let x = 0; x < song.length; x++) {
     lyrics.innerHTML += `
     <ul>
@@ -40,3 +40,47 @@ async function displayLyrics(song, artist, images) {
     `
   }
 }
+//This is for the top 10's list
+let num = 0;
+async function getTopSongs(){
+  let songs = document.querySelector('.top-songs');
+  let response = await fetch("https://shazam.p.rapidapi.com/charts/track?locale=en-US&listId=ip-country-chart-CA&pageSize=20&startFrom=0", {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-key": "4616b7ae9cmshb6e506f5ffff27ep1fe324jsn702dc6119fbe",
+      "x-rapidapi-host": "shazam.p.rapidapi.com"
+    }
+  });
+  let data = await response.json();
+  console.log(data.tracks) 
+  console.log(songs)
+  data.tracks.length = 10
+  data.tracks.forEach(song => {
+    num++;
+    songs.innerHTML += `
+      <h2>${num}.</h2>
+      <img src="${song.images.coverarthq}" width="200" height="200"/>
+      <h3>${song.title}</h3>
+      <h3>${song.subtitle}</h3>
+    `
+  });
+}
+getTopSongs();
+//THIS IS THE HTML CODE
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//   <script src="app.js" defer></script>
+//   <title>Document</title>
+// </head>
+// <body>
+//   <h1>Top 10</h1>
+//   <div>
+//     <ul class="top-songs">
+
+//     </ul>
+//   </div>
+// </body>
+// </html>
