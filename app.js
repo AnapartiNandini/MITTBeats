56697c156fd16f99e8e7d92d5d9082c475b8a2e3
Aryan Saini <aryansaini1005@gmail.com>
2021-04-07 13:36:38 -0500

    working live serach next task implemeting music

--- app.js
+++ app.js
@@ -1,42 +1,49 @@
 //TEMPORARY 
-let lyrics = document.querySelector('.lyrics')
-let artistName = [];
-let songName = [];
-let images = [];
-let newArray;
-async function getArtists() {
-  let response = await fetch("https://genius.p.rapidapi.com/search?q=katy%20perry", {
+const lyrics = document.querySelector('.lyrics')
+const form = document.querySelector('form');
+const searchBar = document.querySelector('#search-bar');
+
+async function getArtists(str) {
+  console.log(str);
+  str = str.split(" ").join("%20");
+
+  let data = await fetch(`https://genius.p.rapidapi.com/search?q=${str}`, {
     "method": "GET",
     "headers": {
       "x-rapidapi-key": "4616b7ae9cmshb6e506f5ffff27ep1fe324jsn702dc6119fbe",
       "x-rapidapi-host": "genius.p.rapidapi.com"
     }
   });
-  let data = await response.json();
-  console.log(data.response.hits)
+  data = await data.json();
+  lyrics.innerHTML = "";
   data.response.hits.forEach(song => {
-    songName.push(song.result.title_with_featured);
-    artistName.push(song.result.primary_artist.name);
-    images.push(song.result.header_image_thumbnail_url)
+    let songName = song.result.title_with_featured;
+    let artistName = song.result.primary_artist.name;
+    let images = song.result.header_image_thumbnail_url;
+      lyrics.innerHTML += `
+      <ul>
+        <img src = "${images}"/>
+        <li class="song-name">
+        ${songName}
+        </li>
+        <li class="artist-name">
+        ${artistName}
+        </li>
+      </ul>
+      `
   });
-  displayLyrics(songName, artistName, images)
-  //formatArtist()
 }
 
-getArtists();
-
-async function displayLyrics(song, artist, images) {
-  for(let x = 0; x < song.length; x++) {
-    lyrics.innerHTML += `
-    <ul>
-      <img src = "${images[x]}"/>
-      <li class="song-name">
-      ${song[x]}
-      </li>
-      <li class="artist-name">
-      ${artist[x]}
-      </li>
-    </ul>
-    `
+searchBar.onkeyup  = (e) => {
+  if(searchBar.value.length >= 3){
+    getArtists(searchBar.value);
   }
 }
+
+form.onsubmit = (e) => {
+  if(searchBar.value.length > 0){
+    getArtists(searchBar.value);
+  }
+  e.preventDefault();
+}
+
