85bd0ddfe564badf4273b6a9bc6e74f30b9c8725
Aryan Saini <aryansaini1005@gmail.com>
2021-04-08 12:36:07 -0500

    Quick javascript merge

--- app.js
+++ app.js
@@ -1,82 +1,119 @@
 //TEMPORARY 
-let lyrics = document.querySelector('.lyrics')
-let input = document.querySelector('form input')
-let form = document.querySelector('form')
-let artistName = [];
-let songName = [];
-let images = [];
-let newArray;
-let value;
-console.log(input)
+const lyrics = document.querySelector('.lyrics')
+const form = document.querySelector('form');
+const searchBar = document.querySelector('#search-bar');
+const baseUrl = { shaz: "https://shazam.p.rapidapi.com/", genius: "https://genius.p.rapidapi.com/", lyrics: "https://api.lyrics.ovh/v1/", download:""};
+const iframe = document.querySelector("iframe");
+const songs = document.querySelector('.top-songs');
 
-form.addEventListener('submit', async function(e) {
-  e.preventDefault();
-  songName = [];
-  artistName = [];
-  images = [];
-  value = input.value;
-  await getArtists();
+navigator.geolocation.getCurrentPosition(function(ite){
 
-});
+}, function(ite){
 
-async function getArtists() {
-  let response = await fetch(`https://genius.p.rapidapi.com/search?q=${value}`, {
-    "method": "GET",
-    "headers": {
-      "x-rapidapi-key": "4616b7ae9cmshb6e506f5ffff27ep1fe324jsn702dc6119fbe",
-      "x-rapidapi-host": "genius.p.rapidapi.com"
-    }
-  });
-  let data = await response.json();
-  console.log(data.response.hits)
-  data.response.hits.forEach(song => {
-    songName.push(song.result.title_with_featured);
-    artistName.push(song.result.primary_artist.name);
-    images.push(song.result.header_image_thumbnail_url)
-  });
-  displayLyrics(songName, artistName, images)
-}
+}, {enableHighAccuracy:true})
 
-async function displayLyrics(song, artist, images) {
-  for(let x = 0; x < song.length; x++) {
-    lyrics.innerHTML += `
-    <ul>
-      <img src = "${images[x]}"/>
-      <li class="song-name">
-      ${song[x]}
-      </li>
-      <li class="artist-name">
-      ${artist[x]}
-      </li>
-    </ul>
-    `
+
+const urlHeaders = {
+  genius1: {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "2247980046msh68a22faa7c63535p109a44jsn81484c7d80bf",
+		"x-rapidapi-host": "genius.p.rapidapi.com"
+	  }
+  }, 
+ genius2: {
+  "method": "GET",
+  "headers": {
+    "x-rapidapi-key": "4616b7ae9cmshb6e506f5ffff27ep1fe324jsn702dc6119fbe",
+    "x-rapidapi-host": "genius.p.rapidapi.com"
   }
-}
-
-//This is for the top 10's list
-let num = 0;
-async function getTopSongs(){
-  let songs = document.querySelector('.top-songs');
-  let response = await fetch("https://shazam.p.rapidapi.com/charts/track?locale=en-US&listId=ip-country-chart-CA&pageSize=20&startFrom=0", {
+  }, 
+  shazam1:{
     "method": "GET",
     "headers": {
       "x-rapidapi-key": "4616b7ae9cmshb6e506f5ffff27ep1fe324jsn702dc6119fbe",
       "x-rapidapi-host": "shazam.p.rapidapi.com"
     }
+  },
+  shazam2:{
+    "method": "GET",
+    "headers": {
+     "x-rapidapi-key": "2247980046msh68a22faa7c63535p109a44jsn81484c7d80bf",
+     "x-rapidapi-host": "shazam.p.rapidapi.com"
+    }
+  }
+}
+
+async function getArtists(str) {
+  str = str.split(" ").join("%20");
+
+  let data = await fetch(`${baseUrl.genius}search?q=${str}`, urlHeaders.genius2);
+  data = await data.json();
+  lyrics.innerHTML = "";
+  data.response.hits.forEach(song => {
+    let songName = song.result.title_with_featured;
+    let artistName = song.result.primary_artist.name;
+    let images = song.result.header_image_thumbnail_url;
+    let id = song.result.id;
+      lyrics.innerHTML += `
+      <ul data-id="${id}">
+        <img src = "${images}"/>
+        <li class="song-name">
+        ${songName}
+        </li>
+        <li class="artist-name">
+        ${artistName}
+        </li>
+        <li class="song-name">
+        <i class="fas fa-play"></i>
+        </li>
+      </ul>
+      `
   });
-  let data = await response.json();
-  console.log(data.tracks) 
-  console.log(songs)
-  data.tracks.length = 10
-  data.tracks.forEach(song => {
-    num++;
-    songs.innerHTML += `
-      <h2>${num}.</h2>
-      <img src="${song.images.coverarthq}" width="200" height="200"/>
-      <h3>${song.title}</h3>
-      <h3>${song.subtitle}</h3>
-    `
-  });
+}
+
+async function playMusicSample(id){
+  let data = await fetch(`${baseUrl.genius}songs/${id}`, urlHeaders.genius1);
+  data = await data.json();
+  iframe.src = `${data.response.song.apple_music_player_url}`;
+}
+
+searchBar.onkeyup  = (e) => {
+  if(searchBar.value.length >= 3){
+    getArtists(searchBar.value);
+  }
+}
+
+form.onsubmit = (e) => {
+  if(searchBar.value.length > 0){
+    getArtists(searchBar.value);
+  }
+  e.preventDefault();
+}
+
+lyrics.onclick = (e) => {
+  console.log(e.target);
+  let close = e.target.closest("ul");
+  console.log(close);
+  if (close != undefined && e.target.classList.contains("fa-play")) {
+    playMusicSample(close.dataset.id);
+    e.target.classlist.toggle("fa-play", "fa-pause");
+
+  } else if (close != undefined && e.target.classList.contains("fa-pause")){
+    e.target.classlist.toggle("fa-play", "fa-pause");
+  } else if (close != undefined ){
+
+  }
+}
+
+//This is for the top 10's list
+async function getTopSongs(){
+  let data = await fetch(`${baseUrl.shaz}charts/list`, urlHeaders.shazam1);
+  data = await data.json();
+  console.log(data) 
+  
+  
+ 
 }
 getTopSongs();
 //THIS IS THE HTML CODE
@@ -97,3 +134,9 @@ getTopSongs();
 //   </div>
 // </body>
 // </html>
+
+/* songs.innerHTML += `
+<h2>${num}.</h2>
+<img src="${song.images.coverarthq}" width="200" height="200"/>
+<h3>${song.title}</h3>
+<h3>${song.subtitle}</h3> */
